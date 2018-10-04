d7db131b22d0738e94f1ad9d7d30ec325bca04dd
AshishSharma303 <ashishsharma303@hotmail.com>
2018-10-04 16:12:44 +0530

    Add files via upload

---
+++ ubuntu-labs/module5/install-minikube.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get -y update
+apt-get -y upgrade
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+
+chmod +x ./kubectl
+
+mv ./kubectl /usr/local/bin/kubectl
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.24.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/docker-machine-driver-kvm2 && chmod +x docker-machine-driver-kvm2 && sudo mv docker-machine-driver-kvm2 /usr/bin/
+
+apt install -y qemu-kvm libvirt-bin
+
+addgroup libvirtd
+
+adduser $USER libvirtd
+
+minikube start --vm-driver kvm2
+
